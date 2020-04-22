2be28c5b74db227c3aafa4c742cb8832638283da
Edemone <edem.agbenyo@gmail.com>
2020-04-22 16:59:24 +0000

    Updates

---
+++ precache-manifest.0c7b869f7836215e3cba6b1e144c1c48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4909fd20a2e0210e0c936bfc7b858b8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a16d317594c3e07d62a0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c3c53d4ca24639d3480b",
+    "url": "/static/js/2.759b8189.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.759b8189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16d317594c3e07d62a0",
+    "url": "/static/js/main.35cc5463.chunk.js"
+  },
+  {
+    "revision": "e279e083f01dd753a59f",
+    "url": "/static/js/runtime-main.c108b4e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
